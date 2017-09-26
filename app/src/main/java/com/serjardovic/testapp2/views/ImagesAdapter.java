626449800ca4d53748a0426c24923e1ecee3a2cf
Sergey <>
2017-09-26 13:01:03 +0300

    Update

--- app/src/main/java/com/serjardovic/testapp2/views/ImagesAdapter.java
+++ app/src/main/java/com/serjardovic/testapp2/views/ImagesAdapter.java
@@ -41,9 +41,11 @@ public class ImagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     private boolean isFooterEnabled;
     private ImageLoader imageLoader;
     private FileCache fileCache;
+    private MemoryCache memoryCache;
 
     public ImagesAdapter(ArrayList<String> images, int height) {
-        fileCache = FileCache.getInstance(MyApplication.getInstance().getApplicationContext());
+        fileCache = FileCache.getInstance(MyApplication.getInstance());
+        memoryCache = MemoryCache.getInstance();
         mImages = new ArrayList<>(images);
         mHeight = height;
         imageLoader = new ImageLoader();
@@ -130,47 +132,62 @@ public class ImagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     private class ImageLoader {
 
         private Map<ImageView, String> imageViews = Collections.synchronizedMap(new WeakHashMap<ImageView, String>());
+        private ExecutorService executorService;
+
 
         public ImageLoader() {
+            executorService = Executors.newFixedThreadPool(MyApplication.getInstance().getNumberOfCores() - 1);
         }
+
 
         public void DisplayImage(String url, ImageView imageView) {
 
             imageViews.put(imageView, url);
-            Bitmap bitmap = getBitmap(url);
-            if (bitmap != null) {
+            Bitmap bitmap = memoryCache.get(url);
+            if (bitmap != null)
                 imageView.setImageBitmap(bitmap);
+            else {
+                queuePhoto(url, imageView);
             }
-
         }
 
+        private void queuePhoto(String url, ImageView imageView) {
+
+            PhotoToLoad p = new PhotoToLoad(url, imageView);
+            executorService.submit(new PhotosLoader(p));
+        }
 
         private Bitmap getBitmap(String imageURL) {
+            File f = fileCache.getFile(imageURL);
 
-            File file = fileCache.getFile(imageURL);
-            Bitmap bitmap = decodeFile(file);
-            if (bitmap != null) {
-                return bitmap;
+            //from SD cache
+            Bitmap b = decodeFile(f);
+            if (b != null) {
+                return b;
             } else {
-                if (!imageURL.contains("Image not found")) {
+                if(!imageURL.contains("Image not found")) {
                     L.d("File is corrupt: " + imageURL + ". Deleting the file and re-downloading...");
                     fileCache.getFile(imageURL).delete();
-
+                    // TODO
                 }
             }
             return null;
         }
 
         //decodes image and scales it to reduce memory consumption
-        private Bitmap decodeFile(File file) {
+        private Bitmap decodeFile(File f) {
             try {
                 //decode image size
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeStream(new FileInputStream(file), null, options);
+                BitmapFactory.Options o = new BitmapFactory.Options();
+                o.inJustDecodeBounds = true;
+                BitmapFactory.decodeStream(new FileInputStream(f), null, o);
 
-                final int REQUIRED_SIZE = MyApplication.getInstance().getDisplayHeight() / 2;
-                int width_tmp = options.outWidth, height_tmp = options.outHeight;
+                //o.outWidth = MainActivity.screenWidth;
+                //o.outHeight = 2 * (o.outWidth / 3);
+
+                //Find the correct scale value. It should be the power of 2.
+                final int REQUIRED_SIZE = MyApplication.getInstance().getDisplayWidth() / 2;
+                int width_tmp = o.outWidth, height_tmp = o.outHeight;
                 int scale = 1;
 
                 while (true) {
@@ -180,14 +197,73 @@ public class ImagesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                     height_tmp /= 0.5;
                     scale *= 2;
                 }
+
+
                 //decode with inSampleSize
-                BitmapFactory.Options options2 = new BitmapFactory.Options();
-                options2.inSampleSize = scale;
-                return BitmapFactory.decodeStream(new FileInputStream(file), null, options2);
+                BitmapFactory.Options o2 = new BitmapFactory.Options();
+                o2.inSampleSize = scale;
+                return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
             return null;
+        }
+
+        //Task for the queue
+        private class PhotoToLoad {
+            String url;
+            ImageView imageView;
+
+            PhotoToLoad(String u, ImageView i) {
+                url = u;
+                imageView = i;
+            }
+        }
+
+        private class PhotosLoader implements Runnable {
+            PhotoToLoad photoToLoad;
+
+            PhotosLoader(PhotoToLoad photoToLoad) {
+                this.photoToLoad = photoToLoad;
+            }
+
+            @Override
+            public void run() {
+                if (imageViewReused(photoToLoad))
+                    return;
+                Bitmap bmp = getBitmap(photoToLoad.url);
+                memoryCache.put(photoToLoad.url, bmp);
+                if (imageViewReused(photoToLoad))
+                    return;
+                BitmapDisplayer bd = new BitmapDisplayer(bmp, photoToLoad);
+                Activity a = (Activity) photoToLoad.imageView.getContext();
+                a.runOnUiThread(bd);
+            }
+        }
+
+        private boolean imageViewReused(PhotoToLoad photoToLoad) {
+            String tag = imageViews.get(photoToLoad.imageView);
+            return (tag == null || !tag.equals(photoToLoad.url));
+        }
+
+        //Used to display bitmap in the UI thread
+        private class BitmapDisplayer implements Runnable {
+            Bitmap bitmap;
+            PhotoToLoad photoToLoad;
+
+            BitmapDisplayer(Bitmap b, PhotoToLoad p) {
+                bitmap = b;
+                photoToLoad = p;
+            }
+
+            public void run() {
+                if (imageViewReused(photoToLoad))
+                    return;
+                if (bitmap != null) {
+                    photoToLoad.imageView.setImageBitmap(bitmap);
+                }
+
+            }
         }
     }
 }
